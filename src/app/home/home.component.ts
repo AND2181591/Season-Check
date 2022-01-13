bcb15893f4e1aeec694b2178205f4d106c1513f6
Andrew <AND2181591@maricopa.edu>
2022-01-12 21:02:10 -0700

    Added the home component and the basic logic to get the season

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  month = new Date().getMonth();
+  season = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        this.season = this.getSeason(lat, this.month);
+      }, 
+      err => console.log(err)
+    );
+  }
+
+  getSeason(lat: number, month: number): string {
+    if (month === 11 || month < 2) {
+      return lat > 0 ? 'winter' : 'summer';
+    } else if (month > 1 && month < 5) {
+      return lat > 0 ? 'spring' : 'fall';
+    } else if (month > 4 && month < 8) {
+      return lat > 0 ? 'summer' : 'winter';
+    } else {
+      return lat > 0 ? 'fall' : 'spring'
+    }
+  }
+}
