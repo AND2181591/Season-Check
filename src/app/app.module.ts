1e4214f2929fc7cfcd0e75753c207c41ce9fd2c3
Andrew <AND2181591@maricopa.edu>
2022-01-15 12:23:10 -0700

    Added a spinner component to show when the user is being prompted to allow or block their location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SeasonDisplayComponent } from './season-display/season-display.component';
 import { ErrorComponent } from './error/error.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SeasonDisplayComponent,
-    ErrorComponent
+    ErrorComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
